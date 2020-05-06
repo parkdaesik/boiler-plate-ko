35526e0905221025cbfaa3821f54039a6cdfda9f
daesik <daesik.park@ahha.tf>
2020-05-06 17:27:05 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 const port = 4000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://daesik:daesik1234@cluster0-w8xfz.gcp.mongodb.net/test?retryWrites=true&w=majority', {
@@ -9,6 +15,20 @@ mongoose.connect('mongodb+srv://daesik:daesik1234@cluster0-w8xfz.gcp.mongodb.net
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello Wolrd!~~안녕하세요~~'));
+app.get('/', (req, res) => res.send('Hello Wolrd!~~안녕하세요~~sss'));
+
+app.post('/register', (req, res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면 
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'));
